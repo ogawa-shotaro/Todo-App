e71d913c2eb2910ba083ff5f4d3d2690d3564f08
shotaro ogawa <ogawashotaro@gmail.com>
2024-04-06 08:43:03 +0900

    Crated findAllMethod or TestFile

---
+++ models/Todo.js
@@ -0,0 +1,27 @@
+const todos = [];
+let nextId = 1;
+
+class Todo {
+  constructor({ title, body }) {
+    this.id = nextId++;
+    this.title = title;
+    this.body = body;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
+
+for (let i = 0; i < 5; i++) {
+  const index = i + 1;
+  const todo = new Todo({
+    title: "タイトル" + index,
+    body: "ボディ" + index,
+  });
+  todos.push(todo);
+}
+
+module.exports = {
+  findAll: () => {
+    return todos.slice();
+  },
+};
