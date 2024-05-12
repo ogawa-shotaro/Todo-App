be2db5879851b2d8329b96d47d2e54871d260f90
shotaro ogawa <ogawashotaro@gmail.com>
2024-05-12 17:24:45 +0900

    CreateTodoController Name Change

--- routers/todos.ts
+++ routers/todos.ts
@@ -5,14 +5,14 @@ import { CreateTodoController } from "../controllers/todos/CreateTodoController"
 import { GetTodosController } from "../controllers/todos/GetTodosController";
 import { GetTodoController } from "../controllers/todos/GetTodoController";
 const todoRepository = new TodoRepository();
-const todoController = new CreateTodoController(todoRepository);
+const todoCreateController = new CreateTodoController(todoRepository);
 const todosGetController = new GetTodosController(todoRepository);
 const todoGetController = new GetTodoController(todoRepository);
 
 router
   .route("/")
   .post((req, res) => {
-    todoController.create(req, res);
+    todoCreateController.create(req, res);
   })
   .get((req, res) => {
     todosGetController.list(req, res);
