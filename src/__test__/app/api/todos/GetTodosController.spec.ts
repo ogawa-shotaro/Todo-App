be33e7fb87a4610c5979badbee052c1d94f0877a
shotaro ogawa <ogawashotaro@gmail.com>
2024-07-16 17:28:54 +0900

    modified-getTodosControllerTestFile-addCodeBeforeEach

--- src/__test__/app/api/todos/GetTodosController.spec.ts
+++ src/__test__/app/api/todos/GetTodosController.spec.ts
@@ -1,9 +1,11 @@
+import { requestAPI } from "../../../helper/requestHelper";
+import type { TodoResponseType } from "../../../helper/types/TodoResponseType";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 describe("[APIテスト] Todo一覧取得", () => {
-  it("成功パターン(prisma使用)", async () => {
+  beforeEach(async () => {
     for (let i = 1; i <= 3; i++) {
       await prisma.todo.create({
         data: {
@@ -12,20 +14,40 @@ describe("[APIテスト] Todo一覧取得", () => {
         },
       });
     }
+  });
+  it("データがある状態でTodo一覧を取得する", async () => {
+    const response = await requestAPI({
+      method: "get",
+      endPoint: "/api/todos",
+      statusCode: 200,
+    });
 
-    const todos = await prisma.todo.findMany();
+    const todoItems: TodoResponseType[] = response.body;
 
-    expect(todos.length).toEqual(3);
-    expect(todos.map((todo) => todo.id)).toEqual([1, 2, 3]);
-    expect(todos.map((todo) => todo.title)).toEqual([
+    expect(todoItems.length).toEqual(3);
+    expect(todoItems.map((todo) => todo.id)).toEqual([1, 2, 3]);
+    expect(todoItems.map((todo) => todo.title)).toEqual([
       "ダミータイトル1",
       "ダミータイトル2",
       "ダミータイトル3",
     ]);
-    expect(todos.map((todo) => todo.body)).toEqual([
+    expect(todoItems.map((todo) => todo.body)).toEqual([
       "ダミーボディ1",
       "ダミーボディ2",
       "ダミーボディ3",
     ]);
+  });
+  it("データがない状態でTodo一覧を取得する", async () => {
+    await prisma.todo.deleteMany({});
+
+    const response = await requestAPI({
+      method: "get",
+      endPoint: "/api/todos",
+      statusCode: 200,
+    });
+
+    const todoItems: TodoResponseType[] = response.body;
+
+    expect(todoItems).toEqual([]);
   });
 });
