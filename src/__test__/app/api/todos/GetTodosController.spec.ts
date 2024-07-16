e570f4b0e06df2f3f31b411b3225b579c9b959e5
shotaro ogawa <ogawashotaro@gmail.com>
2024-07-16 21:20:46 +0900

    modiied describe and itFunction code

--- src/__test__/app/api/todos/GetTodosController.spec.ts
+++ src/__test__/app/api/todos/GetTodosController.spec.ts
@@ -5,49 +5,51 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 describe("[APIテスト] Todo一覧取得", () => {
-  beforeEach(async () => {
-    for (let i = 1; i <= 3; i++) {
-      await prisma.todo.create({
-        data: {
-          title: "ダミータイトル" + i,
-          body: "ダミーボディ" + i,
-        },
+  describe("DBにデータあり", () => {
+    beforeEach(async () => {
+      for (let i = 1; i <= 3; i++) {
+        await prisma.todo.create({
+          data: {
+            title: "ダミータイトル" + i,
+            body: "ダミーボディ" + i,
+          },
+        });
+      }
+    });
+    it("データがある状態でTodo一覧を取得する", async () => {
+      const response = await requestAPI({
+        method: "get",
+        endPoint: "/api/todos",
+        statusCode: 200,
       });
-    }
-  });
-  it("データがある状態でTodo一覧を取得する", async () => {
-    const response = await requestAPI({
-      method: "get",
-      endPoint: "/api/todos",
-      statusCode: 200,
+
+      const todoItems: TodoResponseType[] = response.body;
+
+      expect(todoItems.length).toEqual(3);
+      expect(todoItems.map((todo) => todo.id)).toEqual([1, 2, 3]);
+      expect(todoItems.map((todo) => todo.title)).toEqual([
+        "ダミータイトル1",
+        "ダミータイトル2",
+        "ダミータイトル3",
+      ]);
+      expect(todoItems.map((todo) => todo.body)).toEqual([
+        "ダミーボディ1",
+        "ダミーボディ2",
+        "ダミーボディ3",
+      ]);
     });
-
-    const todoItems: TodoResponseType[] = response.body;
-
-    expect(todoItems.length).toEqual(3);
-    expect(todoItems.map((todo) => todo.id)).toEqual([1, 2, 3]);
-    expect(todoItems.map((todo) => todo.title)).toEqual([
-      "ダミータイトル1",
-      "ダミータイトル2",
-      "ダミータイトル3",
-    ]);
-    expect(todoItems.map((todo) => todo.body)).toEqual([
-      "ダミーボディ1",
-      "ダミーボディ2",
-      "ダミーボディ3",
-    ]);
   });
-  it("データがない状態でTodo一覧を取得する", async () => {
-    await prisma.todo.deleteMany({});
+  describe("DBにデータなし", () => {
+    it("データがない状態でTodo一覧を取得する", async () => {
+      const response = await requestAPI({
+        method: "get",
+        endPoint: "/api/todos",
+        statusCode: 200,
+      });
 
-    const response = await requestAPI({
-      method: "get",
-      endPoint: "/api/todos",
-      statusCode: 200,
+      const todoItems: TodoResponseType[] = response.body;
+
+      expect(todoItems).toEqual([]);
     });
-
-    const todoItems: TodoResponseType[] = response.body;
-
-    expect(todoItems).toEqual([]);
   });
 });
