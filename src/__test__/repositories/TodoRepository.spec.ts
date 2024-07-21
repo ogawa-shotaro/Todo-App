022f4df326aacf0cde2fa7ff64d8ca09b42fd97a
shotaro ogawa <ogawashotaro@gmail.com>
2024-07-21 12:47:41 +0900

    refactored Variable name

--- src/__test__/repositories/TodoRepository.spec.ts
+++ src/__test__/repositories/TodoRepository.spec.ts
@@ -17,27 +17,27 @@ describe("TodoRepository", () => {
       it("saveメソッドを実行すると、DBに値を保持し、その値に重複しないIDが付与される", async () => {
         const repository = new TodoRepository();
 
-        const initialDataResult: Todo = await repository.save({
+        const initialTodo: Todo = await repository.save({
           title: "ダミータイトル1",
           body: "ダミーボディ1",
         });
 
-        const secondDataResult: Todo = await repository.save({
+        const secondTodo: Todo = await repository.save({
           title: "ダミータイトル2",
           body: "ダミーボディ2",
         });
 
-        expect(initialDataResult.id).toEqual(1);
-        expect(initialDataResult.title).toEqual("ダミータイトル1");
-        expect(initialDataResult.body).toEqual("ダミーボディ1");
-        expect(initialDataResult.createdAt).toBeInstanceOf(Date);
-        expect(initialDataResult.updatedAt).toBeInstanceOf(Date);
+        expect(initialTodo.id).toEqual(1);
+        expect(initialTodo.title).toEqual("ダミータイトル1");
+        expect(initialTodo.body).toEqual("ダミーボディ1");
+        expect(initialTodo.createdAt).toBeInstanceOf(Date);
+        expect(initialTodo.updatedAt).toBeInstanceOf(Date);
 
-        expect(secondDataResult.id).toEqual(2);
-        expect(secondDataResult.title).toEqual("ダミータイトル2");
-        expect(secondDataResult.body).toEqual("ダミーボディ2");
-        expect(secondDataResult.createdAt).toBeInstanceOf(Date);
-        expect(secondDataResult.updatedAt).toBeInstanceOf(Date);
+        expect(secondTodo.id).toEqual(2);
+        expect(secondTodo.title).toEqual("ダミータイトル2");
+        expect(secondTodo.body).toEqual("ダミーボディ2");
+        expect(secondTodo.createdAt).toBeInstanceOf(Date);
+        expect(secondTodo.updatedAt).toBeInstanceOf(Date);
       });
     });
     describe("list・update・deleteメソッドのテスト", () => {
@@ -53,101 +53,99 @@ describe("TodoRepository", () => {
       });
       it("listメソッドを実行時、パラメーターの指定がない場合は、先頭から10件のデータを取得する", async () => {
         const repository = new TodoRepository();
-        const listDataResult = await repository.list();
+        const todoList = await repository.list();
 
-        expect(listDataResult.length).toEqual(10);
-        expect(listDataResult[2].id).toEqual(3);
-        expect(listDataResult[2].title).toEqual("ダミータイトル3");
-        expect(listDataResult[2].body).toEqual("ダミーボディ3");
+        expect(todoList.length).toEqual(10);
+        expect(todoList[2].id).toEqual(3);
+        expect(todoList[2].title).toEqual("ダミータイトル3");
+        expect(todoList[2].body).toEqual("ダミーボディ3");
       });
       it("listメソッドを実行時、パラメーターの指定(page=2)をした場合、11件目のデータから20件のデータを取得する", async () => {
         const repository = new TodoRepository();
-        const listDataResult = await repository.list({ page: 2 });
+        const todoList = await repository.list({ page: 2 });
 
-        expect(listDataResult.length).toEqual(10);
-        expect(listDataResult[0].id).toEqual(11);
-        expect(listDataResult[0].title).toEqual("ダミータイトル11");
-        expect(listDataResult[0].body).toEqual("ダミーボディ11");
+        expect(todoList.length).toEqual(10);
+        expect(todoList[0].id).toEqual(11);
+        expect(todoList[0].title).toEqual("ダミータイトル11");
+        expect(todoList[0].body).toEqual("ダミーボディ11");
       });
       it("listメソッドを実行時、パラメーターの指定(count=5)をした場合、先頭から5件のデータを取得する", async () => {
         const repository = new TodoRepository();
-        const listDataResult = await repository.list({ count: 5 });
+        const todoList = await repository.list({ count: 5 });
 
-        expect(listDataResult.length).toEqual(5);
-        expect(listDataResult[4].id).toEqual(5);
-        expect(listDataResult[4].title).toEqual("ダミータイトル5");
-        expect(listDataResult[4].body).toEqual("ダミーボディ5");
+        expect(todoList.length).toEqual(5);
+        expect(todoList[4].id).toEqual(5);
+        expect(todoList[4].title).toEqual("ダミータイトル5");
+        expect(todoList[4].body).toEqual("ダミーボディ5");
       });
       it("listメソッドを実行時、パラメーターの指定(page=2,count=3)をした場合、4件目のデータから3件のデータを取得する", async () => {
         const repository = new TodoRepository();
-        const listDataResult = await repository.list({ page: 2, count: 3 });
+        const todoList = await repository.list({ page: 2, count: 3 });
 
-        expect(listDataResult.length).toEqual(3);
-        expect(listDataResult[0].id).toEqual(4);
-        expect(listDataResult[0].title).toEqual("ダミータイトル4");
-        expect(listDataResult[0].body).toEqual("ダミーボディ4");
+        expect(todoList.length).toEqual(3);
+        expect(todoList[0].id).toEqual(4);
+        expect(todoList[0].title).toEqual("ダミータイトル4");
+        expect(todoList[0].body).toEqual("ダミーボディ4");
       });
       it("findメソッドを実行すると、DBに保持されているデータから、一件の値を取得する事ができる", async () => {
         const repository = new TodoRepository();
 
-        const initialDataResult = await repository.find(1);
-        const secondDataResult = await repository.find(2);
+        const initialTodo = await repository.find(1);
+        const secondTodo = await repository.find(2);
 
-        expect(initialDataResult?.id).toEqual(1);
-        expect(initialDataResult?.title).toEqual("ダミータイトル1");
-        expect(initialDataResult?.body).toEqual("ダミーボディ1");
-        expect(initialDataResult?.createdAt).toBeInstanceOf(Date);
-        expect(initialDataResult?.updatedAt).toBeInstanceOf(Date);
+        expect(initialTodo?.id).toEqual(1);
+        expect(initialTodo?.title).toEqual("ダミータイトル1");
+        expect(initialTodo?.body).toEqual("ダミーボディ1");
+        expect(initialTodo?.createdAt).toBeInstanceOf(Date);
+        expect(initialTodo?.updatedAt).toBeInstanceOf(Date);
 
-        expect(secondDataResult?.id).toEqual(2);
-        expect(secondDataResult?.title).toEqual("ダミータイトル2");
-        expect(secondDataResult?.body).toEqual("ダミーボディ2");
-        expect(secondDataResult?.createdAt).toBeInstanceOf(Date);
-        expect(secondDataResult?.updatedAt).toBeInstanceOf(Date);
+        expect(secondTodo?.id).toEqual(2);
+        expect(secondTodo?.title).toEqual("ダミータイトル2");
+        expect(secondTodo?.body).toEqual("ダミーボディ2");
+        expect(secondTodo?.createdAt).toBeInstanceOf(Date);
+        expect(secondTodo?.updatedAt).toBeInstanceOf(Date);
       });
       it("updateメソッドを実行すると、DB内のデータを更新する事ができる。", async () => {
         const repository = new TodoRepository();
 
-        const latestResult: Todo = await repository.update({
+        const updatedTodo: Todo = await repository.update({
           id: 1,
           title: "変更後のタイトル",
           body: "変更後のボディ",
         });
 
-        expect(latestResult).toEqual({
+        expect(updatedTodo).toEqual({
           id: 1,
           title: "変更後のタイトル",
           body: "変更後のボディ",
-          createdAt: latestResult.createdAt,
-          updatedAt: latestResult.updatedAt,
+          createdAt: updatedTodo.createdAt,
+          updatedAt: updatedTodo.updatedAt,
         });
       });
       it("updateメソッドを実行した後は、updatedAtの方がcreatedAtよりも新しい時間になっている。", async () => {
         const repository = new TodoRepository();
 
-        const latestDateResult = await repository.update({
+        const updatedTodo = await repository.update({
           id: 1,
           title: "変更後のタイトル",
           body: "変更後のボディ",
         });
 
-        expect(
-          latestDateResult.createdAt < latestDateResult.updatedAt
-        ).toBeTruthy();
+        expect(updatedTodo.createdAt < updatedTodo.updatedAt).toBeTruthy();
       });
       it("deleteメソッドを実行すると、DB内の指定した(ID)データを削除する事ができる。", async () => {
         const repository = new TodoRepository();
 
-        const dbOldDataResult = await repository.list();
-        const deletedDataResult = await repository.delete(1);
-        const dbCurrentDataResult = await repository.list();
+        const oldTodos = await repository.list();
+        const deletedTodo = await repository.delete(1);
+        const newTodos = await repository.list();
 
-        const oldDataId = dbOldDataResult.map((todo) => todo.id);
-        const currentDataId = dbCurrentDataResult.map((todo) => todo.id);
+        const oldTodoIds = oldTodos.map((todo) => todo.id);
+        const newTodoIds = newTodos.map((todo) => todo.id);
 
-        expect(deletedDataResult.id).toEqual(1);
-        expect(oldDataId).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-        expect(currentDataId).toEqual([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+        expect(deletedTodo.id).toEqual(1);
+        expect(oldTodoIds).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        expect(newTodoIds).toEqual([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
       });
     });
   });
