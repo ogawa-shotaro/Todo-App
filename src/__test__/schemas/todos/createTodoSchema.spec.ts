94e24e6debfd7b0d76ccb3820646db2fbc4e13db
shotaro ogawa <ogawashotaro@gmail.com>
2024-09-08 15:11:14 +0900

    code review after modified to createTodoSchema test file

--- src/__test__/schemas/todos/createTodoSchema.spec.ts
+++ src/__test__/schemas/todos/createTodoSchema.spec.ts
@@ -1,31 +1,103 @@
 import { ZodError } from "zod";
 
 import { createTodoSchema } from "../../../schemas/todos/createTodoSchema";
-import { createMockRequest } from "../../helper/mocks/request";
 
 describe("【ユニットテスト】createTodoSchemaの挙動テスト", () => {
   describe("【成功パターン】", () => {
     it("【バリデーションに成功した場合】例外が発生しない。", () => {
-      const req = createMockRequest({
-        body: { title: "ダミータイトル", body: "ダミーボディ" },
-      });
-
-      const mockParse = jest.spyOn(createTodoSchema, "parse");
-      createTodoSchema.parse(req);
-
-      expect(mockParse).toHaveBeenCalledWith(req);
-    });
-  });
-  describe("【異常パターン】", () => {
-    it("【バリデーションに失敗した場合】例外(ZodError)が発生する。", () => {
-      const req = {
+      const validateData = {
         body: {
-          title: "",
-          body: "",
+          title: "ダミータイトル",
+          body: "ダミーボディ",
         },
       };
 
-      expect(() => createTodoSchema.parse(req)).toThrow(ZodError);
+      const result = createTodoSchema.parse(validateData);
+
+      expect(result).toEqual(validateData);
+    });
+  });
+  describe("【異常パターン】", () => {
+    it("【titleプロパティの入力値がない場合】例外(required_error)が発生する。", () => {
+      const validateData = {
+        body: {
+          body: "ダミーボディ",
+        },
+      };
+
+      expect(() => createTodoSchema.parse(validateData)).toThrow(
+        new ZodError([
+          {
+            code: "invalid_type",
+            expected: "string",
+            received: "undefined",
+            path: ["body", "title"],
+            message: "titleの内容は必須です。",
+          },
+        ]),
+      );
+    });
+    it("【titleプロパティ有り・入力値(1文字以上)がない場合】例外(min_error)が発生する。", () => {
+      const validateData = {
+        body: {
+          title: "",
+          body: "ダミーボディ",
+        },
+      };
+
+      expect(() => createTodoSchema.parse(validateData)).toThrow(
+        new ZodError([
+          {
+            code: "too_small",
+            minimum: 1,
+            type: "string",
+            inclusive: true,
+            exact: false,
+            message: "titleは1文字以上である必要があります。",
+            path: ["body", "title"],
+          },
+        ]),
+      );
+    });
+    it("【bodyプロパティの入力値がない場合】例外(required_error)が発生する。", () => {
+      const validateData = {
+        body: {
+          title: "ダミータイトル",
+        },
+      };
+
+      expect(() => createTodoSchema.parse(validateData)).toThrow(
+        new ZodError([
+          {
+            code: "invalid_type",
+            expected: "string",
+            received: "undefined",
+            path: ["body", "body"],
+            message: "bodyの内容は必須です。",
+          },
+        ]),
+      );
+    });
+    it("【bodyプロパティ有り・入力値(1文字以上)がない場合】例外(min_error)が発生する。", () => {
+      const validateData = {
+        body: {
+          title: "ダミータイトル",
+          body: "",
+        },
+      };
+      expect(() => createTodoSchema.parse(validateData)).toThrow(
+        new ZodError([
+          {
+            code: "too_small",
+            minimum: 1,
+            type: "string",
+            inclusive: true,
+            exact: false,
+            message: "bodyは1文字以上である必要があります。",
+            path: ["body", "body"],
+          },
+        ]),
+      );
     });
   });
 });
