4c2f3edbe0f8713c49037fcb36bae6e1aeac4ca7
shotaro ogawa <ogawashotaro@gmail.com>
2024-09-10 19:19:53 +0900

    code review after modified to getTodosSchema testFile

--- src/__test__/schemas/todos/getTodosSchema.spec.ts
+++ src/__test__/schemas/todos/getTodosSchema.spec.ts
@@ -5,42 +5,35 @@ import { getTodosSchema } from "../../../schemas/todos/getTodosSchema";
 describe("【ユニットテスト】getTodosSchemaの挙動テスト", () => {
   describe("【成功パターン】", () => {
     it("【バリデーションに成功した場合】例外が発生しない。", () => {
-      const data = {
-        query: { page: "1", count: "5" },
-      };
+      const data = { query: { page: "1", count: "5" } };
+      const result = getTodosSchema.parse(data);
 
-      const result = getTodosSchema.parse(data.query);
-
-      expect(result).toEqual(data.query);
+      expect(result).toEqual(data);
     });
   });
   describe("【異常パターン】", () => {
     it("【pageの入力値が不正(page=整数の1以上でない値)な場合】例外が発生する。", () => {
-      const data = {
-        query: { page: "0" },
-      };
+      const data = { query: { page: "0" } };
 
-      expect(() => getTodosSchema.parse(data.query)).toThrow(
+      expect(() => getTodosSchema.parse(data)).toThrow(
         new ZodError([
           {
             code: "custom",
             message: "pageは1以上の整数のみ。",
-            path: ["page"],
+            path: ["query", "page"],
           },
         ]),
       );
     });
     it("【countの入力値が不正(count=整数の1以上でない値)な場合】例外が発生する。", () => {
-      const data = {
-        query: { count: "0" },
-      };
+      const data = { query: { count: "0" } };
 
-      expect(() => getTodosSchema.parse(data.query)).toThrow(
+      expect(() => getTodosSchema.parse(data)).toThrow(
         new ZodError([
           {
             code: "custom",
             message: "countは1以上の整数のみ。",
-            path: ["count"],
+            path: ["query", "count"],
           },
         ]),
       );
