2b7d33cc53405163037f081ef1956454274a858a
shotaro ogawa <ogawashotaro@gmail.com>
2024-09-29 09:56:45 +0900

    Add custom error types in TypeScript

--- src/types/TodoResponse.typs.ts
+++ src/types/TodoResponse.typs.ts
@@ -1,4 +1,9 @@
 import type { InvalidError } from "../errors/InvalidError";
 import type { NotFoundError } from "../errors/NotFoundError";
+import type { UnauthorizedError } from "../errors/UnauthorizedError";
 
-export type ResponseErrorType = InvalidError | NotFoundError | Error;
+export type ResponseErrorType =
+  | InvalidError
+  | NotFoundError
+  | UnauthorizedError
+  | Error;
