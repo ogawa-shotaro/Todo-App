ac36420ee9406fde54da94da893911ee68e32ab9
shotaro ogawa <ogawashotaro@gmail.com>
2024-10-27 22:41:29 +0900

    modified loginUserSchema

--- src/schemas/users/loginUserSchema.ts
+++ src/schemas/users/loginUserSchema.ts
@@ -3,7 +3,5 @@ import { z } from "zod";
 import { emailPasswordSchema } from "./shared/emailPasswordSchema";
 
 export const loginUserSchema = z.object({
-  body: z.object({
-    ...emailPasswordSchema.shape,
-  }),
+  body: emailPasswordSchema,
 });
