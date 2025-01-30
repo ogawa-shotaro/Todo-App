90e81a1ce189fc7552f4ebf0dc783176344ee8d9
shotaro ogawa <ogawashotaro@gmail.com>
2025-01-30 19:35:06 +0900

    Fix again before code review

--- backend/src/controllers/todos/GetTodosController.ts
+++ backend/src/controllers/todos/GetTodosController.ts
@@ -6,14 +6,18 @@ import type { AuthenticatedRequest } from "../../types/requests/AuthenticatedReq
 
 export class GetTodosController {
   private repository: ITodoRepository;
+
   constructor(repository: ITodoRepository) {
     this.repository = repository;
   }
+
   async list(req: AuthenticatedRequest, res: Response, next: NextFunction) {
     const page = req.query.page ? Number(req.query.page) : undefined;
     const count = req.query.count ? Number(req.query.count) : undefined;
+
     try {
       const todos = await this.repository.list({ page, count });
+
       res.status(StatusCodes.OK).json(todos);
     } catch (error) {
       next(error);
